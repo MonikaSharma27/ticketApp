401011cd8b29fb83269d0dc7db283d513f2e0670
Aadesh Kumar <116622502+aadesh-kumarr@users.noreply.github.com>
2025-03-13 23:37:49 +0530

    form improvements

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -4,7 +4,6 @@ import { useForm } from "react-hook-form";
 import axios from "axios";
 import SignupRight from "./signupRight";
 
-
 const Signup = () => {
   const {
     register,
@@ -32,31 +31,27 @@ const Signup = () => {
   };
 
   return (
-    <div className=" bg-[#2b3441] sm:bg-[#47566a] h-screen sm:p-16 p-7 sm:px-28">
-      
-      
-      <div className="flex sm:bg-white h-full items-center justify-center rounded-md ">
-        <div className=" text-[#2b3441] sm:w-[50%] sm:p-10 xl:px-20 ">
-          <div
-            className="bg-white w-full p-6  rounded-md  "
-          >
-            <h1 className="text-3xl font-bold mb-10 mt-10 text-center lg:mt-10 lg:text-5xl">
+    <div className="bg-[#2b3441] sm:bg-[#47566a] h-screen sm:p-16 p-4 sm:px-28">
+      <div className="flex sm:bg-white h-full items-center justify-center rounded-md">
+        <div className="text-[#2b3441] sm:w-[50%] sm:p-10 xl:px-20 p-4">
+          <div className="bg-white w-full p-6 rounded-md">
+            <h1 className="text-3xl font-bold mb-6 mt-6 text-center lg:mt-10 lg:text-5xl">
               Sign up
             </h1>
             <form
               action=""
-              className="flex flex-col "
+              className="flex flex-col"
               onSubmit={handleSubmit(onSubmit)}
             >
-              <div className="mb-3 ">
+              <div className="mb-3">
                 <input
                   type="text"
                   placeholder="Full Name"
                   {...register("fullname", { required: true })}
-                  className="outline-none border-2 border-gray-300 w-full  rounded-md py-1 px-2 mb-4 bg-gray-200"
+                  className="outline-none border-2 border-gray-300 w-full rounded-md py-1 px-2 mb-4 bg-gray-200"
                 />
                 {errors.fullname && (
-                  <span className="text-sm text-red-500 ">
+                  <span className="text-sm text-red-500">
                     This field is required{" "}
                   </span>
                 )}
@@ -70,13 +65,13 @@ const Signup = () => {
                   className="outline-none border-2 border-gray-300 w-full rounded-md py-1 px-2 mb-4 bg-gray-200"
                 />
                 {errors.email && (
-                  <span className="text-sm text-red-500 ">
+                  <span className="text-sm text-red-500">
                     This field is required{" "}
                   </span>
                 )}
               </div>
 
-              <div className=" mb-8">
+              <div className="mb-8">
                 <input
                   type="password"
                   placeholder="Password"
@@ -84,21 +79,27 @@ const Signup = () => {
                   className="outline-none border-2 border-gray-300 w-full rounded-md py-1 px-2 bg-gray-200"
                 />
                 {errors.password && (
-                  <span className="text-sm text-red-500 ">
+                  <span className="text-sm text-red-500">
                     This field is required{" "}
                   </span>
                 )}
               </div>
 
-              <button className="bg-[#2b3441] text-white p-1 text-2xl font-bold rounded-md 
-              hover:bg-white hover:text-[#2b3441] hover:border-2 hover:border-[#2b3441] ">Signup</button>
-              <p className='text-end mt-2 sm:hidden text-sm'>Already have an account ? <Link to="/" className='text-lg font-bold'>Login</Link></p>
+              <button className="bg-[#2b3441] text-white p-1 text-2xl font-bold rounded-md hover:bg-white hover:text-[#2b3441] hover:border-2 hover:border-[#2b3441]">
+                Signup
+              </button>
+              <p className="text-end mt-2 sm:hidden text-sm">
+                Already have an account?{" "}
+                <Link to="/" className="text-lg font-bold">
+                  Login
+                </Link>
+              </p>
             </form>
           </div>
         </div>
 
-        <div className="w-[50%] bg-[#2b3441]  h-full rounded-bl-[25%] rounded-tl-[25%] hidden sm:block rounded-tr-md rounded-br-md">
-          <SignupRight/>
+        <div className="w-[50%] bg-[#2b3441] h-full rounded-bl-[25%] rounded-tl-[25%] hidden sm:block rounded-tr-md rounded-br-md">
+          <SignupRight />
         </div>
       </div>
     </div>
